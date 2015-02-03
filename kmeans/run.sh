9ee628b14e03fec74e1cb3dc778301bf28361b6a
Edward ZILONG HUANG <zilongh@andrew.cmu.edu>
2015-02-03 16:01:06 -0500

    what's happening?

--- kmeans/run.sh
+++ kmeans/run.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-./omp_main -i kmeans01.dat -n 3 -o
-./omp_main -i kmeans02.dat -n 3 -o
-./omp_main -i kmeans03.dat -n 3 -o
-./omp_main -i kmeans04.dat -n 3 -o
+./omp_main -i kmeans01.dat -n 3 -o -a
+./omp_main -i kmeans02.dat -n 3 -o -a
+./omp_main -i kmeans03.dat -n 3 -o -a
+./omp_main -i kmeans04.dat -n 3 -o -a
