72671f638c65fbb883619103b0b9e40959351426
seckcoder <seckcoder@gmail.com>
2015-02-03 17:17:45 -0800

    loop unrolling

--- matrix_mul/omp/matrix_mul.cpp
+++ matrix_mul/omp/matrix_mul.cpp
@@ -47,96 +47,78 @@ inline uint greatestBitValue(uint x) {
 namespace omp
 {
 
+  void matrix_multiplication(
+      float *sq_matrix_1,
+      float *sq_matrix_2,
+      float *sq_matrix_result,
+      unsigned int sq_dimension ) {
+    // seems we can't modify sq_matrix_1 and sq_matrix_2....
 
+    bool aligned = true;
+    uint mask4 = 0x3;
+    // use bit operation to determin whether sq_dimension is multiple of 4
+    if ((sq_dimension & mask4) > 0) {
+      // dimension that's a multiple of 4
+      uint dim4 = sq_dimension + 4 - (sq_dimension & mask4);
 
-
-void
-    matrix_multiplication(
-        float *sq_matrix_1,
-        float *sq_matrix_2,
-        float *sq_matrix_result,
-        unsigned int sq_dimension ) {
-      // seems we can't modify sq_matrix_1 and sq_matrix_2....
-
-      assert(uintptr_t(sq_matrix_1) % 16 == 0);
-      uint mask4 = 0x3;
-      if ((sq_dimension & mask4) > 0) {
-        uint dim4 = sq_dimension + 4 - (sq_dimension & mask4);
-        float *sq1 = (float *)malloc(dim4*dim4*sizeof(float));
-        float *sq2_t = (float *)malloc(dim4*dim4*sizeof(float));
-        assert(uintptr_t(sq1) % 16 == 0);
-        assert(uintptr_t(sq2_t) % 16 == 0);
-        for (uint i = 0; i < sq_dimension; i++) {
-          uint base_i4 = i*dim4;
-          uint base_i = i*sq_dimension;
-          for (uint j = 0; j < sq_dimension; j++) {
-            sq1[base_i4 + j] = sq_matrix_1[base_i+j];
-            sq2_t[j*dim4 + i] = sq_matrix_2[base_i+j];
-          }
-          for (uint j = sq_dimension; j < dim4; j++) {
-            sq1[base_i4 + j] = 0;
-            sq2_t[j*dim4 + i] = 0;
-          }
+      // sq1 is the aligned version of sq_matrix_1
+      float *sq1 = (float *)calloc(dim4*dim4, sizeof(float));
+      // sq2_t is the transform of sq_matrix_2(also aligned)
+      float *sq2_t = (float *)calloc(dim4*dim4, sizeof(float));
+      for (uint i = 0; i < sq_dimension; i++) {
+        uint base_i4 = i*dim4;
+        uint base_i = i*sq_dimension;
+        for (uint j = 0; j < sq_dimension; j++) {
+          // align sq_matrix_1 data
+          sq1[base_i4 + j] = sq_matrix_1[base_i+j];
+          // Transform sq_matrix_2
+          sq2_t[j*dim4 + i] = sq_matrix_2[base_i+j];
         }
-        for (uint i = sq_dimension; i < dim4; i++) {
-          uint base_i4 = i*dim4;
-          uint base_i = i*sq_dimension;
-          for (uint j = 0; j < sq_dimension; j++) {
-            sq1[base_i4 + j] = sq_matrix_1[base_i+j];
-            sq2_t[j*dim4 + i] = sq_matrix_2[base_i+j];
-          }
-          for (uint j = sq_dimension; j < dim4; j++) {
-            sq1[base_i4 + j] = 0;
-            sq2_t[j*dim4 + i] = 0;
-          }
-        }
-#pragma omp parallel for
-        for (uint i = 0; i < sq_dimension; i++) {
-          for (uint j = 0; j < sq_dimension; j++) {
-            __m128 sum128 = _mm_setzero_ps();
-            __m128 X, Y;
-            uint base_i4 = i * dim4;
-            uint base_j4 = j * dim4;
-            for (uint k = 0; k < sq_dimension; k+=4) {
-              X = _mm_load_ps(&sq1[base_i4+k]);
-              Y = _mm_load_ps(&sq2_t[base_j4+k]);
-              sum128 = _mm_add_ps(sum128, _mm_mul_ps(X, Y));
-            }
-            float sum_temp[4];
-            _mm_store_ps(sum_temp, sum128);
-            float sum_32 = sum_temp[0] + sum_temp[1] + sum_temp[2] + sum_temp[3];
-            sq_matrix_result[i*sq_dimension+j] = sum_32;
-          }
-        }
-        free(sq1);
-        free(sq2_t);
-      } else {
-        float *sq2_t = (float *)malloc(sq_dimension*sq_dimension*sizeof(float));
-        for (uint i = 0; i < sq_dimension; i++) {
-          uint base_i = i * sq_dimension;
-          for (uint j = 0; j < sq_dimension; j++) {
-            sq2_t[j*sq_dimension+i ] = sq_matrix_2[base_i+j];
-          }
-        }
-#pragma omp parallel for
-        for (uint i = 0; i < sq_dimension; i++) {
-          for (uint j = 0; j < sq_dimension; j++) {
-            __m128 sum128 = _mm_setzero_ps();
-            __m128 X, Y;
-            uint base_i = i * sq_dimension;
-            uint base_j = j * sq_dimension;
-            for (uint k = 0; k < sq_dimension; k+=4) {
-              X = _mm_load_ps(&sq_matrix_1[base_i+k]);
-              Y = _mm_load_ps(&sq2_t[base_j+k]);
-              sum128 = _mm_add_ps(sum128, _mm_mul_ps(X,Y));
-            }
-            float sum_temp[4];
-            _mm_store_ps(sum_temp, sum128);
-            float sum32 = sum_temp[0] + sum_temp[1] + sum_temp[2] + sum_temp[3];
-            sq_matrix_result[i*sq_dimension + j] = sum32;
-          }
-        }
-        free(sq2_t);
       }
+      aligned = false;
+#pragma omp parallel for
+      for (uint i = 0; i < sq_dimension; i++) {
+        for (uint j = 0; j < sq_dimension; j++) {
+          float sum = 0;
+          uint base_i4 = i * dim4;
+          uint base_j4 = j * dim4;
+          for (uint k = 0; k < sq_dimension; k += 4) {
+            sum += sq1[base_i4+0] * sq2_t[base_j4+0]
+            sum += sq1[base_i4+1] * sq2_t[base_j4+1]
+            sum += sq1[base_i4+2] * sq2_t[base_j4+2]
+            sum += sq1[base_i4+3] * sq2_t[base_j4+3]
+          }
+          sq_matrix_result[i*sq_dimension+j] = sum;
+        }
+      }
+      free(sq1);
+      free(sq2_t);
+    } else {
+      float *sq1 = sq_matrix_1;
+      // transform of sq_matrix_2
+      float *sq2_t = (float *)malloc(sq_dimension*sq_dimension*sizeof(float));
+      for (uint i = 0; i < sq_dimension; i++) {
+        uint base_i = i * sq_dimension;
+        for (uint j = 0; j < sq_dimension; j++) {
+          sq2_t[j*sq_dimension+i] = sq_matrix_2[base_i+j];
+        }
+      }
+#pragma omp parallel for
+      for (uint i = 0; i < sq_dimension; i++) {
+        for (uint j = 0; j < sq_dimension; j++) {
+          float sum = 0;
+          uint base_i = i * sq_dimension;
+          uint base_j = j * sq_dimension;
+          for (uint k = 0; k < sq_dimension; k+=4) {
+            sum += sq1[base_i+0] * sq2_t[base_j+0]
+            sum += sq1[base_i+1] * sq2_t[base_j+1]
+            sum += sq1[base_i+2] * sq2_t[base_j+2]
+            sum += sq1[base_i+3] * sq2_t[base_j+3]
+          }
+          sq_matrix_result[i*sq_dimension + j] = sum;
+        }
+      }
+      free(sq2_t);
     }
+  }
 } //namespace omp
